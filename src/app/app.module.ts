2f9560f5812901e76062cc8691028c917244fece
dymafr <contact@dyma.fr>
2022-12-06 13:59:32 +0100

    maj angular 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -8,7 +8,6 @@ import { MaterialModule } from './shared/modules/material.module';
 import { StoreModule } from '@ngrx/store';
 import { ROOT_REDUCERS } from './shared/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +17,10 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     MaterialModule,
     StoreModule.forRoot(ROOT_REDUCERS),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+    }),
   ],
   bootstrap: [AppComponent],
 })
